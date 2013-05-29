1c440e4897d7f62ee9b1d05ef606f8549ecb96d7
Maciej Machulak <maciej.machulak@gmail.com>
2013-05-29 23:17:18 +0100

    jiscmail branding

--- scriptage.js
+++ scriptage.js
@@ -20,27 +20,7 @@ function get_hashtag() {
 }
 
 function clog(message) {
-    console.log("Twitter Scout ~> " + message);
-}
-
-function hashtag_search(hashtag) {
-    var url = 'https://search.twitter.com/search.json?q=' + encodeURIComponent(hashtag);
-    /* X-Origin */
-    url += "&callback=?";
-
-    $.ajax(url, {
-        dataType: "json",
-        success: function(data) {
-            clog("API call succeeded.");
-            console.log(data);
-            default_parser(data);
-        },
-        error: function(data) {
-            clog("Oops. API call borked.");
-            console.log(data);
-            default_parser(data);
-        },
-    });
+    console.log("JISCmail ~> " + message);
 }
 
 function default_parser(data) {
@@ -77,34 +57,6 @@ function messagebox(message, description) {
     $('#mbox_description').html(description);
 }
 
-function render_results(data) {
-    clog("in render_results()");
-    console.log(data.results.length);
-    for (var i = 0; i < data.results.length; i++) {
-        var ne = $('.clone-model').clone(true); // new entry
-        ne.appendTo($('#feed'));
-        ne.removeClass('clone-model');
-        ne.find('img').attr('src', data.results[i].profile_image_url_https);
-        ne.find('.author-username').text(data.results[i].from_user_name);
-        ne.find('.author-handle').html('<a class="twitter_handle" target="_blank" href="https://twitter.com/' + data.results[i].from_user + '">@' + data.results[i].from_user + '</a>') ;
-        ne.find('.post-content').html(magic_spell(data.results[i].text));
-        dbg = ne.find('img');
-    }
-    $('#feed').show();
-    clog("exiting render_results()");
-}
-
-function conjure_tweet_button(ht) {
-    console.log(ht);
-    var proto_1 = "<a href='https://twitter.com/intent/tweet?button_hashtag=";
-    var proto_2 = "' class='twitter-hashtag-button' data-size='large'>Tweet ";
-    var proto_3 = "</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>";
-
-    var btn = proto_1 + ht.substr(1) + proto_2 + ht + proto_3;
-    $('#twitterbutton').empty();
-    $('#twitterbutton').append($(btn));
-}
-
 function entry() {
     /* enlarge your widget. satisfy your user. */
     gadgets.window.adjustHeight(295);
@@ -116,19 +68,12 @@ function entry() {
         } else if (ev.data != current_group) {
             current_group = ev.data;
             var group_name = ev.data.split(":");
-            var hashtag = "#" + group_name[group_name.length-1]; 
-            /* insert twitter button */
-            conjure_tweet_button(hashtag);
-            hashtag_search(hashtag);
-            setInterval(function() {
-                clog("15 seconds up. Updating feed.");
-                hashtag_search(hashtag);
-            }, 15000);        
+            group_name = group_name[group_name.length-1];
         } else {
             clog("no changes required, same group.");
         }
     });
 
-    top.postMessage("let's go!", "http://portaldev.cloud.jiscadvance.biz"); 
+    top.postMessage("let's go!", top.location.origin);
 }
 
