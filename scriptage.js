a5e5992dff3e22dd081228d9a3563d92a406889f
Maciej Machulak <maciej.machulak@gmail.com>
2013-05-30 11:09:14 +0100

    branding

--- scriptage.js
+++ scriptage.js
@@ -49,7 +49,7 @@ function entry() {
             clog("Your group: " + group_name);
             showLink(group_name);
             $('#divRss').FeedEk({
-                FeedUrl : 'https://www.jiscmail.ac.uk/cgi-bin/webadmin?RSS&v=2.0&L=' + group_name,
+                FeedUrl : 'https://www.jiscmail.ac.uk/cgi-bin/webadmin?RSS&v=2.0&L=AAHPN',
                 MaxCount : 4,
                 ShowDesc : true,
                 ShowPubDate:true,
