905d77423fd0e01ab8416abc71c4f0289ba17cdf
Maciej Machulak <maciej.machulak@gmail.com>
2013-05-30 11:08:42 +0100

    branding

--- scriptage.js
+++ scriptage.js
@@ -2,38 +2,8 @@ var current_group;
 var response;
 var dbg;
 
-function magic_spell(text) {
-    /* Pieces commented out because I wasn't able to test it. */
-    var hashtag_exp = /#([a-zA-Z0-9]+)/g;
-    var mention_exp = /@([a-zA-Z0-9]+)/g;
-    var href_exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    text = text.replace(href_exp, "<a href='$1' target='_blank'>$1</a>"); 
-    text = text.replace(hashtag_exp, '<a href="https://twitter.com/search?q=%23$1" target="_blank">#$1</a>');
-    text = text.replace(mention_exp, '<a href="https://twitter.com/$1" target="_blank">@$1</a>');
-    return text;
-}
-
-function get_hashtag() {
-    var group_name = current_group.split(":");
-    var hashtag = "#" + group_name[group_name.length-1]; 
-    return hashtag;
-}
-
 function clog(message) {
     console.log("JISCmail ~> " + message);
-}
-
-function default_parser(data) {
-    decommission_splash();
-    response = data;
-    clog("in default_parser()");
-    if (data.results.length == 0) {
-        clog("no results.");
-        render_empty_feed();
-    } else {
-        clog("> 0 results");
-        render_results(data);
-    }
 }
 
 function decommission_splash() {
@@ -78,6 +48,14 @@ function entry() {
             group_name = group_name[group_name.length-1];
             clog("Your group: " + group_name);
             showLink(group_name);
+            $('#divRss').FeedEk({
+                FeedUrl : 'https://www.jiscmail.ac.uk/cgi-bin/webadmin?RSS&v=2.0&L=' + group_name,
+                MaxCount : 4,
+                ShowDesc : true,
+                ShowPubDate:true,
+                DescCharacterLimit:100,
+                TitleLinkTarget:'_blank'
+            });
         } else {
             clog("no changes required, same group.");
         }
