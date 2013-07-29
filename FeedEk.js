e337a4f464fd6d93bb18fff7edd2f34d687fd629
Maciej Machulak <maciej.machulak@gmail.com>
2013-07-29 03:23:19 +0200

    https

--- FeedEk.js
+++ FeedEk.js
@@ -20,7 +20,7 @@
         var i;
         $("#" + id).empty().append('<img src="https://jiscmail-widget.identitylabs.org/loader.gif" />');
         $.ajax({
-            url: "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + def.MaxCount + "&output=json&q=" + encodeURIComponent(def.FeedUrl) + "&hl=en&callback=?",
+            url: "https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + def.MaxCount + "&output=json&q=" + encodeURIComponent(def.FeedUrl) + "&hl=en&callback=?",
             dataType: "json",
             success: function (data) {
                 $("#" + id).empty();
